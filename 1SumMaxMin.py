35a575cac1be16a5d180e58fab5680e7a0ef8f1f
AagamShah <78716556+AagamShah2314@users.noreply.github.com>
2024-06-08 21:21:18 +0530

    Create 1SumMaxMin.py

---
+++ 1SumMaxMin.py
@@ -0,0 +1,19 @@
+//1.Sum Of Max And Min
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def sumOfMaxMin(arr):
+    # Write your code here
+    max=arr[0]
+    min=arr[0]
+    for i in range(0,len(arr)):
+        if min>arr[i]:
+            min=arr[i]
+        if max<arr[i]:
+            max=arr[i]
+    return min+max
+    pass
+
