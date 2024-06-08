f1cb78b2902cb3251cf310f7b74161dbb024a343
AagamShah <78716556+AagamShah2314@users.noreply.github.com>
2024-06-08 21:09:46 +0530

    Create 0RevArr.py
    0th  problem of array

---
+++ 0RevArr.py
@@ -0,0 +1,32 @@
+//Reversing an array
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def reverseArray(arr, m):
+    # Write your code here.
+    l=len(arr)
+    a=arr[:m+1]
+    b=arr[m+1:l]
+    b=b[::-1]
+    return a+b
+    pass
+
+#taking inpit using fast I/O
+def takeInput() :
+	
+    n =  int(input())
+
+    if(n == 0) :
+        return list(), n
+
+    arr = list(map(int, stdin.readline().strip().split(" ")))
+
+    return arr, n
+
+
+#main
+arr, n = takeInput()
+print(maxSubarraySum(arr, n))
