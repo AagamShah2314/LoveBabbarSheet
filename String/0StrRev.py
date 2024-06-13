a5e5b9270546a5c9afb385d8f2ef04f6196e7694
AagamShah <78716556+AagamShah2314@users.noreply.github.com>
2024-06-13 22:36:09 +0530

    Create 0StrRev.py

---
+++ String/0StrRev.py
@@ -0,0 +1,12 @@
+# 0.String Reverse
+
+from sys import *
+from collections import *
+from math import *
+
+from typing import *
+
+def stringReverse(S: str)-> str:
+    # Write your Code here
+    return S[::-1]
+    pass
